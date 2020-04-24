79113e4eed3dd75f0f507ad76d4680312aa738db
Mehul Shah <mehul.developer2019@gmail.com>
2020-04-24 19:07:43 +0530

    added list

--- app/src/scenes/FirstScreen.js
+++ app/src/scenes/FirstScreen.js
@@ -1,26 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {TouchableOpacity, FlatList} from 'react-native-gesture-handler';
 import Axios from 'axios';
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 var api_key = 'Gm0GowOLicT74YPJ2SDXZ8pir4Nylj5HAqo3yBty';
 
 const FirstScreen = ({route, navigation}) => {
-  const [data1, setData1] = useState({id: ''});
+  const [data1, setData1] = useState({id: '', validation: true});
   const [datasource, setDatasource] = useState([]);
-  let arrData = [];
+  const [response, setResponse] = useState('');
+  const [validation, setValidation] = useState(true);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [arrData, setarrData] = useState([]);
+  //let arrData = [];
   let randomItem = [];
 
   const apiCall = () => {
     Axios.get(
       `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${api_key}`,
     ).then(res => {
-      console.log('REsult', res.data.near_earth_objects);
+      console.log('REsult', res.data.near_earth_objects[0].id);
+      setarrData(res.data.near_earth_objects);
       var resultarray = res.data.near_earth_objects;
 
       for (let i = 0; i < resultarray[i].id.length - 1; i++) {
@@ -29,26 +35,51 @@ const FirstScreen = ({route, navigation}) => {
       console.log('arrData', arrData);
       randomItem = arrData[Math.floor(Math.random() * arrData.length)];
       console.log('randomItem', randomItem);
+      if (res.status == 200) {
+        setDataLoaded(true);
+      }
     });
   };
 
+  const apiCall1 = () => {
+    Axios.get(
+      `https://api.nasa.gov/neo/rest/v1/neo/${data1.id}?api_key=${api_key}`,
+    )
+      .then(res => {
+        console.log('REsult', res), setResponse(res.data);
+        if (res.status == 200) {
+          navigation.navigate('Second', {id: res.data.id});
+        } else {
+          alert('No ID Found');
+        }
+      })
+      .catch(error => alert('no id found'));
+  };
+
   useEffect(() => {
-    apiCall();
+    //apiCall();
+    if (arrData.length > 0) {
+      setDataLoaded(true);
+    }
+
+    console.log('arrd', arrData);
   }, []);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <TextInput
         placeholderTextColor="#000"
         style={styles.textInputStyle}
         placeholder="Enter Asteroid' ID"
         autoCapitalize="none"
         autoCorrect={false}
-        onChangeText={id => setData1(id)}
+        onChangeText={id => setData1({id, validation: false})}
         // value={id}
       />
-
       <TouchableOpacity
-        onPress={() => navigation.navigate('Second', {id: data1})}
+        onPress={() => apiCall1()}
+        // navigation.navigate('Second', {id: data1}, apiCall1())}
+        disabled={data1.validation}
         style={styles.buttonStyle}>
         <Text
           style={{
@@ -60,9 +91,9 @@ const FirstScreen = ({route, navigation}) => {
           Submit
         </Text>
       </TouchableOpacity>
-
       <TouchableOpacity
-        onPress={() => navigation.navigate('Second', {id: randomItem})}
+        onPress={() => apiCall()}
+        // onPress={() => navigation.navigate('Second', {id: randomItem})}
         style={styles.buttonStyle}>
         <Text
           style={{
@@ -74,7 +105,27 @@ const FirstScreen = ({route, navigation}) => {
           Random Asteroid
         </Text>
       </TouchableOpacity>
-    </View>
+      {dataLoaded ? (
+        <View style={{marginHorizontal: 30, marginBottom: 30}}>
+          <FlatList
+            data={arrData}
+            renderItem={({item}) => {
+              console.log('item', arrData);
+              return (
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('Second', {id: item.id})}>
+                  <View style={{height: 30, marginTop: 20}}>
+                    <Text style={{color: '#000'}}>{item.id}</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+      ) : (
+        <Text>Not loaded</Text>
+      )}
+    </SafeAreaView>
   );
 };
 
@@ -83,8 +134,8 @@ export default FirstScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    //justifyContent: 'center',
+    //alignItems: 'center',
   },
   textInputStyle: {
     borderWidth: 1.5,
